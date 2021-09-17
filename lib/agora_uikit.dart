ac496223ab801b1fbb81c31608c724fd1b515640
Tadas Petra <tadas@tadaspetra.com>
2021-09-17 13:47:47 -0500

    Added in all the types from Agora SDK

--- lib/agora_uikit.dart
+++ lib/agora_uikit.dart
@@ -1,12 +1,3 @@
-export 'src/agora_client.dart' show AgoraClient;
-export 'src/layout/layout.dart' show AgoraVideoViewer;
-export 'src/buttons/buttons.dart' show AgoraVideoButtons;
-export 'models/agora_connection_data.dart' show AgoraConnectionData;
-export 'models/agora_event_handlers.dart' show AgoraEventHandlers;
-export 'models/agora_channel_data.dart' show AgoraChannelData;
-export 'src/enums.dart';
-
-export 'package:permission_handler/permission_handler.dart';
 export 'package:agora_rtc_engine/rtc_engine.dart'
     show
         VideoEncoderConfiguration,
@@ -28,4 +19,39 @@ export 'package:agora_rtc_engine/rtc_engine.dart'
         AudioLocalError,
         LocalVideoStreamState,
         LocalVideoStreamError,
-        AreaCode;
+        AreaCode,
+        WarningCode,
+        UserInfo,
+        ConnectionStateType,
+        ConnectionChangedReason,
+        NetworkType,
+        AudioOutputRouting,
+        Rect,
+        NetworkQuality,
+        LastmileProbeResult,
+        LocalVideoStats,
+        LocalAudioStats,
+        RemoteVideoStats,
+        RemoteAudioStats,
+        AudioMixingStateCode,
+        AudioMixingReason,
+        RtmpStreamingState,
+        RtmpStreamingErrorCode,
+        InjectStreamStatus,
+        ChannelMediaRelayState,
+        ChannelMediaRelayError,
+        ChannelMediaRelayEvent,
+        StreamPublishState,
+        StreamSubscribeState,
+        RtmpStreamingEvent,
+        SuperResolutionStateReason,
+        UploadErrorReason;
+export 'package:permission_handler/permission_handler.dart';
+
+export 'models/agora_channel_data.dart' show AgoraChannelData;
+export 'models/agora_connection_data.dart' show AgoraConnectionData;
+export 'models/agora_event_handlers.dart' show AgoraEventHandlers;
+export 'src/agora_client.dart' show AgoraClient;
+export 'src/buttons/buttons.dart' show AgoraVideoButtons;
+export 'src/enums.dart';
+export 'src/layout/layout.dart' show AgoraVideoViewer;
