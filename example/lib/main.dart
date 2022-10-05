fd49bd00ff14e560d48d0f6480c206cf092b7b25
Joshua Erinosho <joshuaerinosho@gmail.com>
2022-10-05 05:29:28 +0100

    Upgrade Flutter SDK in sample app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -6,7 +6,7 @@ void main() {
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({Key key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   State<MyApp> createState() => _MyAppState();
