5d6d07c009d03c7f787003d8027956780efe6203
Meherdeep <meherdeept@gmail.com>
2023-06-14 01:03:06 +0530

    update uikit version

--- lib/models/agora_rtm_mute_request.dart
+++ lib/models/agora_rtm_mute_request.dart
@@ -99,7 +99,7 @@ class AgoraUIKit {
   String platform = platformStr();
 
   String framework = "flutter";
-  String version = "1.3.3";
+  String version = "1.3.4";
 
   AgoraUIKit.fromJson(Map<String, dynamic> json)
       : platform = json['platform'],
