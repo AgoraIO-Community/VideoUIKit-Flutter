59b2f10a1f3717b761f63883f3680abe5e898d8a
Tadas Petra <tadas@tadaspetra.com>
2024-03-04 09:32:30 -0600

    Update type exports

--- lib/agora_uikit.dart
+++ lib/agora_uikit.dart
@@ -16,10 +16,10 @@ export 'package:agora_rtc_engine/agora_rtc_engine.dart'
         RemoteAudioState,
         RemoteAudioStateReason,
         LocalAudioStreamState,
-        LocalAudioStreamError,
+        LocalAudioStreamReason,
         AudioVolumeInfo,
         LocalVideoStreamState,
-        LocalVideoStreamError,
+        LocalVideoStreamReason,
         AreaCode,
         UserInfo,
         ConnectionStateType,
@@ -34,11 +34,10 @@ export 'package:agora_rtc_engine/agora_rtc_engine.dart'
         AudioMixingStateType,
         AudioMixingReasonType,
         RtmpStreamPublishState,
-        RtmpStreamPublishErrorType,
+        RtmpStreamPublishReason,
         InjectStreamStatus,
         ChannelMediaRelayState,
         ChannelMediaRelayError,
-        ChannelMediaRelayEvent,
         StreamPublishState,
         StreamSubscribeState,
         RtmpStreamingEvent,
@@ -63,16 +62,15 @@ export 'package:agora_rtm/agora_rtm.dart'
         RtmPeerOnlineState,
         RtmPeerSubscriptionOption,
         RtmRemoteInvitationState;
-
 export 'package:permission_handler/permission_handler.dart';
 
 export 'models/agora_channel_data.dart' show AgoraChannelData;
 export 'models/agora_connection_data.dart' show AgoraConnectionData;
 export 'models/agora_rtc_event_handlers.dart' show AgoraRtcEventHandlers;
-export 'models/agora_rtm_client_event_handler.dart'
-    show AgoraRtmClientEventHandler;
 export 'models/agora_rtm_channel_event_handler.dart'
     show AgoraRtmChannelEventHandler;
+export 'models/agora_rtm_client_event_handler.dart'
+    show AgoraRtmClientEventHandler;
 export 'src/agora_client.dart' show AgoraClient;
 export 'src/buttons/buttons.dart' show AgoraVideoButtons;
 export 'src/enums.dart';
